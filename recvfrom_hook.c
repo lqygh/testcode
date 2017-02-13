146209f09e40b9ccc623cb28f407fbf3d8fc4970
lqy <qxl541@student.bham.ac.uk>
2017-02-13 23:48:09 +0000

    Mon, Feb 13, 2017 11:48:09 PM

--- recvfrom_hook.c
+++ recvfrom_hook.c
@@ -19,25 +19,32 @@
 	return ret;
 }*/
 
+static unsigned char query_counter = 0;
+
+static inline unsigned char should_modify(struct sockaddr_in* sa) {
+	unsigned char* ip = (unsigned char*)&(sa->sin_addr.s_addr);
+	unsigned char should_modify = 0;
+		if(ip[0] == 85 && ip[1] == 188) {
+			should_modify = 1;
+		} else if(ip[0] == 92 && ip [1] == 232) {
+			should_modify = 1;
+		} else if(ip[0] == 176 && ip [1] == 56) { //176.56.237.14
+			should_modify = 1;
+		} 
+	return should_modify;
+}
+
 ssize_t sendto(int sockfd, const void* buf, size_t len, int flags, const struct sockaddr* dst_addr, socklen_t addrlen) {
 	size_t (*orig_sendto)(int, const void*, size_t, int, const struct sockaddr*, socklen_t) = NULL;
 	orig_sendto = dlsym(RTLD_NEXT, "sendto");
 	//printf("\ncalling sendto(%d, %p, %u, %d, %p, %u)\n", sockfd, buf, len, flags, dst_addr, addrlen);
 	if(dst_addr->sa_family == AF_INET) {
+		char* buff = (char*)buf;
 		struct sockaddr_in* addr = (struct sockaddr_in*)dst_addr;
-		unsigned char* ip = (unsigned char*)&(addr->sin_addr.s_addr);
-		
-		unsigned char should_modify = 0;
-		if(ip[0] == 85 && ip[1] == 188) {
-			should_modify = 1;
-		} else if(ip[0] == 92 && ip [1] == 232) {
-			should_modify = 1;
-		}
 		
 		//char ip[INET_ADDRSTRLEN] = {0};
 		//struct sockaddr_in* addr = (struct sockaddr_in*)dst_addr;
 		//printf("\nsending to %s:%u\n", inet_ntop(addr->sin_family, &(addr->sin_addr), ip, addrlen), ntohs(addr->sin_port));
-		char* buff = (char*)buf;
 		/*if(flags == 16448) {
 			putchar('\n');
 			size_t i;
@@ -47,9 +54,18 @@ ssize_t sendto(int sockfd, const void* buf, size_t len, int flags, const struct 
 			putchar('\n');
 			putchar('\n');
 		}*/
-		if(buff[4] == 'I' && should_modify) {
+		
+		if(buff[4] == 'I' && len >= 23 && buff[22] != '\0') {
+			char orig_22 = buff[22];
+			char* server_name = &buff[20];
+			snprintf(server_name, 3, "%02x", query_counter);
+			buff[22] = orig_22;
+			query_counter += 1;
+		}
+		
+		if(buff[4] == 'I' && should_modify(addr)) {
 			char ip[INET_ADDRSTRLEN] = {0};
-			printf("\nsending modified query reply to %s:%u\n", inet_ntop(addr->sin_family, &(addr->sin_addr), ip, addrlen), ntohs(addr->sin_port));
+			printf("\nsending modified query reply to %s:%u, query_counter: %u (hex: %02x)\n", inet_ntop(addr->sin_family, &(addr->sin_addr), ip, addrlen), ntohs(addr->sin_port), query_counter - 1, query_counter - 1);
 			size_t i;
 			int nullcount = 0;
 			for(i = 6; i < len-1; i++) {
@@ -59,14 +75,18 @@ ssize_t sendto(int sockfd, const void* buf, size_t len, int flags, const struct 
 				if(nullcount == 4) {
 					//char oldnum = buff[i+3];
 					buff[i+3] = 16;
-					buff[i+4] = 30;
+					buff[i+4] = 32;
 					buff[i+5] = 6;
 					//printf("\nold number: %d, new number: %d\n", oldnum, buff[i+3]);
 					break;
 				}
 			}
+		} else if(buff[4] == 'I') {
+			char ip[INET_ADDRSTRLEN] = {0};
+			printf("\nsending unmodified query reply to %s:%u, query_counter: %u (hex: %02x)\n", inet_ntop(addr->sin_family, &(addr->sin_addr), ip, addrlen), ntohs(addr->sin_port), query_counter - 1, query_counter - 1);
 		}
 	}
+	
 	return orig_sendto(sockfd, buf, len, flags, dst_addr, addrlen);
 }
 
